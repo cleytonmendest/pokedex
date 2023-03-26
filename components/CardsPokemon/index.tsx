0f10571dd20aaa029dbbc8513e20611163b3f7b9
Cleyton Mendes <cleyton.mendest@gmail.com>
2023-03-26 04:51:46 -0300

    ajustes css

--- components/CardsPokemon/index.tsx
+++ components/CardsPokemon/index.tsx
@@ -8,7 +8,7 @@ const CardPokemon = ({ pokemon }: any) => {
     const heightDiv = (height / 10).toLocaleString('pt-BR')
     const weightDiv = (weight / 10).toLocaleString('pt-BR')
 
-    console.log(stats, pokemon.name)
+    const basicStats = stats.filter((stat: any) => stat.stat.name === "hp" || stat.stat.name === "attack" || stat.stat.name === "defense" || stat.stat.name === "speed")
 
     const typeColors: any = {
         normal: '#A8A878',
@@ -39,18 +39,34 @@ const CardPokemon = ({ pokemon }: any) => {
             <div className={styles.idArea}>
                 <p>ID: {id}</p>
             </div>
-            <img className={styles.cardPokemon__image} src={front_default}/>
-            <h3 className={styles.cardPokemon__pokemonName}>{pokemon.name}</h3>
-            <div className={styles.basicsInfo}>
-                <p>Altura: {heightDiv}m</p>
-                <p>Peso: {weightDiv} kg</p>
-            </div>
-            <div className={styles.cardPokemon__typesArea}>
-                {types && types.map((types: any, index: number) => {
-                    const { type } = types
+            <img className={styles.cardPokemon__image} src={front_default} />
+            <div className={styles.generalInfoContainer}>
+                <h3 className={styles.cardPokemon__pokemonName}>{pokemon.name}</h3>
+                <div className={styles.basicsInfoContainer}>
+                    <h5>Informações Básicas</h5>
+                    <div className={styles.basicsInfo}>
+                        <p>Altura: {heightDiv}m</p>
+                        <p>Peso: {weightDiv} kg</p>
+                    </div>
+                </div>
+                <div className={styles.statsInfoContainer}>
+                    <h5>Status Básicos</h5>
+                    <div className={styles.statsInfo}>
+                        {basicStats && basicStats.map((stat: any, index: number) => {
+                            const nameStat = stat.stat.name.toUpperCase()
 
-                    return <p key={index} style={{ backgroundColor: typeColors[type.name] }} className={styles.typesArea__typeName}>{type.name}</p>
-                })}
+                            return (<p key={index}>{nameStat}: {stat.base_stat}</p>)
+                        })}
+                    </div>
+                </div>
+
+                <div className={styles.cardPokemon__typesArea}>
+                    {types && types.map((types: any, index: number) => {
+                        const { type } = types
+
+                        return <p key={index} style={{ backgroundColor: typeColors[type.name] }} className={styles.typesArea__typeName}>{type.name}</p>
+                    })}
+                </div>
             </div>
         </Link>
 
